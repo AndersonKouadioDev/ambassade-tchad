2af34be7fc57d2d85c8c7f6fa0000f52c8227d83
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-08-30 23:27:05 +0000

    default theme light

--- providers/providers.tsx
+++ providers/providers.tsx
@@ -1,9 +1,14 @@
 import { NextuiProvider } from "./nextuiProvider";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <NextuiProvider>{children}</NextuiProvider>
+      <NextuiProvider>
+        <NextThemesProvider attribute="class" defaultTheme="light">
+          {children}
+        </NextThemesProvider>
+      </NextuiProvider>
     </>
   );
 }
